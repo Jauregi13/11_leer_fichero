d29b6c5f2a9c6dd66835d64ffafe8e1624ab408c
Jon Jauregi <Jon Jauregi@DESKTOP-UPFRNRT>
2017-11-07 00:12:08 +0100

    Ejercicio con multiplos

--- src/ejercicio01.java
+++ src/ejercicio01.java
@@ -2,28 +2,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
-
 public class ejercicio01 {
-	
-	/*public static boolean multiplo_de_3 (int[] numeros){
-		for(int i = 0; i < numeros.length; i++){
-			if(numeros[i] % 3 == 0){
-				
+
+	public static boolean multiplo_de_3(int numero) {
+		boolean multiplo = true;
+			if (numero % 2 == 0) {
+				return multiplo = true;
+
 			}
-		}
+			else {
+				return multiplo = false;
+			}
 		
-		return true;
-	}*/
+
+		
+	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		File fichero = new File("D:/mifichero.txt");
+		File fichero = new File("C:/Users/Jon Jauregi/Desktop/datos/mifichero.txt");
 		Scanner scan = new Scanner(fichero);
-		
-			String linea = scan.nextLine();
-			String[] cadena = linea.split(" ");
 
-		for (int i = 0; i < cadena.length ; i++){
+		String linea = scan.nextLine();
+		String[] cadena = linea.split(" ");
+
+		for (int i = 0; i < cadena.length; i++) {
+			int numero = Integer.parseInt(cadena[i]);
+			if (multiplo_de_3(numero)== true){
+				System.out.println("El numero " + numero + " es multiplo de 3");
+			}
 			
 		}
 
